1570d3cc562b0ced65455bb39f4039c3f20533c1
TruongDuyTven <duyvntse170126@fpt.edu.vn>
2023-05-31 14:25:51 +0700

    first day

--- JavaScript/5-const.js
+++ JavaScript/5-const.js
@@ -1,3 +1,13 @@
 // const = a variable that can't be changed
 
-let pi = 3.14159;
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter radius of the circle");
+radius = Number(radius);
+
+//PI = 420 is not working because const can't be changed
+
+circumference = radius * PI * 2;
+console.log("The circumference is ", circumference);
